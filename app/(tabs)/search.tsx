f539206c672010036f0c13a6319436e407647c67
Rajab Shoukath Kunnath <rajabshoukathis@gmail.com>
2025-04-22 18:03:54 +0530

    Enhance search functionality in the movie tab by implementing a search bar, fetching movies based on user input, and displaying results in a FlatList. Added loading and error handling states, and improved UI elements for better user experience.

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,12 +1,87 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image, FlatList, ActivityIndicator } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { images } from '@/constants/images'
+import MovieCard from '@/components/MovieCard'
+import useFetch from '@/services/useFetch'
+import { fetchMovies } from '@/services/api'
+import { useRouter } from 'expo-router'
+import { icons } from '@/constants/icons'
+import SearchBar from '@/components/SearchBar'
 
-const search = () => {
+const Search = () => {
+  const [searchQuery, setSearchQuery] = useState('')
+  const {
+    data: movies,
+    loading,
+    error,
+    refetch: loadMovies,
+    reset
+  } = useFetch(() => fetchMovies({
+    query: searchQuery
+  }), false)
+  useEffect(() => {
+    const timeoutId = setTimeout(
+      async () => {
+        if (searchQuery.trim()) {
+          await loadMovies();
+        } else {
+          reset()
+        }
+      }, 500);
+      return ()=> clearTimeout(timeoutId);
+  }, [searchQuery])
   return (
-    <View>
-      <Text>search</Text>
+    <View className='flex-1 bg-primary'>
+      <Image source={images.bg} className="flex-1 absolute w-full z-0" resizeMode='cover' />
+      <FlatList
+        data={movies}
+        renderItem={({ item }) => <MovieCard {...item}
+        />}
+        keyExtractor={(item) => item.id.toString()}
+        className='px-5'
+        numColumns={3}
+        columnWrapperStyle={{
+          justifyContent: 'center',
+          gap: 16,
+          marginVertical: 16
+        }}
+        contentContainerStyle={{ paddingBottom: 100 }}
+        ListEmptyComponent={
+          !loading && !error?(
+            <View className='mt-10 px-5'>
+              <Text className='text-center text-gray-500'>
+                {searchQuery.trim() ? 'No Movies Found' : "Search for a movie"}
+              </Text>
+            </View>
+          ): null
+        }
+        ListHeaderComponent={
+          <>
+            <View className='w-full flex-row justify-center mt-20 items-center'>
+              <Image source={icons.logo} className='w-12 h-10'></Image>
+            </View>
+            <View className='my-5'>
+              <SearchBar value={searchQuery} placeholder='Search Movies ...' onChangeText={(text) => setSearchQuery(text)} />
+            </View>
+            {loading && (
+              <ActivityIndicator size="large" color='#0000FF' className='my-3' />
+            )}
+            {error && (
+              <Text className='text-red-500 px-5 my-3'>Eroor:{error.message}</Text>
+            )}
+            {
+              !loading && !error && searchQuery.trim() &&
+              movies?.length > 0 && (
+                <Text className='text-xl text-white font-bold'>
+                  Search Results for {''}
+                  <Text className='text-accent'>{searchQuery}</Text>
+                </Text>
+              )}
+          </>
+        }
+      />
     </View>
   )
 }
 
-export default search
+export default Search
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -4,12 +4,14 @@ import { icons } from '@/constants/icons'
 interface Props {
     placeholder: string;
     onPress?: () => void;
+    value?: string;
+    onChangeText?: (text: string) => void;
 }
-const SearchBar = ({ onPress, placeholder }: Props) => {
+const SearchBar = ({ onPress, placeholder, value, onChangeText }: Props) => {
     return (
         <View className='flex-row items-center bg-dark-200 rounded-full px-5 py-4'>
             <Image source={icons.search} className='size-4' resizeMode='contain' tintColor="#ab8bff" />
-            <TextInput onPress={onPress} placeholder={placeholder} value='' onChangeText={() => { }} placeholderTextColor="#a8b5db" className='flex-1 ml-2 text-white' />
+            <TextInput onPress={onPress} placeholder={placeholder} value={value} onChangeText={onChangeText} placeholderTextColor="#a8b5db" className='flex-1 ml-2 text-white' />
         </View>
     )
 }
